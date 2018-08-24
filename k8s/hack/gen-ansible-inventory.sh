76b173a5431aeb498c5c3e6f8ae9a445c67b3d68
Haoyu Wang <wanghaoyu@qiniu.com>
2018-08-24 11:08:22 +0800

    gen-ansible-inventory.sh

--- k8s/hack/gen-ansible-inventory.sh
+++ k8s/hack/gen-ansible-inventory.sh
@@ -6,9 +6,10 @@ kubectl get nodes -ogo-template='{{- range .items}}
       {{- .metadata.name }}{{ "\t" }}
       {{- range .status.addresses }}
           {{- .type }}={{ .address }}{{ "\t" }}
-      {{- end }}{{"\n"}}
+      {{- end }}GPU={{ index .metadata.labels "node-role.qiniu.com/gpu" }}{{"\n"}}
 {{- end}}' | grep InternalIP | while read l; do
-	ip=$(echo "$l" | perl -lne 'print $1 if /InternalIP=([^\s]*)/')
-	hostname=$(echo "$l" | perl -lne 'print $1 if /Hostname=([^\s]*)/')
-	printf '%s ansible_ssh_host=%s ansible_user=root  ansible_connection=ssh ansible_ssh_extra_args="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"\n' "$hostname" "$ip"
+        ip=$(echo "$l" | perl -lne 'print $1 if /InternalIP=([^\s]*)/')
+        hostname=$(echo "$l" | perl -lne 'print $1 if /Hostname=([^\s]*)/')
+        has_gpu=$(echo "$l" | perl -lne 'print "has_gpu=$1" if /GPU=(true)/')
+        printf '%s ansible_ssh_host=%s %s\n' "$hostname" "$ip" "$has_gpu"
 done
